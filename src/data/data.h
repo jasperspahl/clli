b2a1dd766d860563380004b137a31282f42e1c9a
Jasper Spahl <jasperspahl@web.de>
2023-02-16 17:44:19 +0100

    smal change

--- src/data/data.h
+++ src/data/data.h
@@ -15,8 +15,6 @@ typedef struct opensource_project_ {
 opensource_project *
 create_opensource_project(const char *name, const char *description, const char *url, uint32_t stars, uint32_t issues);
 
-//opensource_project *curl_opensource_project(char *url);
-
 void free_opensource_project(void *value);
 
 /**
@@ -26,11 +24,11 @@ void free_opensource_project(void *value);
  * @code{.c}
  * struct osp_buffer {
  * 	size_t name_length;
- * 	char name[name_length];
+ * 	char name[.name_length];
  * 	size_t desc_length;
- * 	char description[desc_length];
+ * 	char description[.desc_length];
  * 	size_t url_length;
- * 	char url[url_length];
+ * 	char url[.url_length];
  * 	uint32_t stars;
  * 	uint32_t issues;
  * };
